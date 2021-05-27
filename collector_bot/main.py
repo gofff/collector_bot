40010f103fdf9b99a007072e7eafec34ea8682fd
gofff <khatiullin@gmail.com>
2021-05-27 21:28:59 +0300

    Add reading token from file

---
+++ collector_bot/main.py
@@ -0,0 +1,17 @@
+import argparse
+import sys
+import os
+
+def read_token(token_filename: str) -> str:
+    with open(token_filename, 'r') as token_file:
+        return token_file.readline().strip()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--token', dest='token_path', action='store',
+                        required=True,
+                        help='path to text file with token string')
+    args = parser.parse_args()
+    token = read_token(args.token_path)
+    print(token)
+    
