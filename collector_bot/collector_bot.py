27507d8596c073bc63ab0126a45912785efc72fc
gofff <khatiullin@gmail.com>
2021-06-02 22:27:07 +0300

    Add creating job as class instance

--- collector_bot/collector_bot.py
+++ collector_bot/collector_bot.py
@@ -1,3 +1,6 @@
+import random
+import uuid
+
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import (
     Updater,
@@ -13,15 +16,10 @@ CREATE, START, PAUSE, EDIT, DELETE, DOWNLOAD = map(chr, range(6))
 SELECTING_ACTION, STOPPING = map(chr, range(7, 9))
 SET_NAME, SET_TIME, SET_QUESTION = map(chr, range(9, 12))
 CHOOSE_NAME = chr(12)
-START_OVER = chr(13)
 
 # Top level conversation callbacks
 def start(update: Update, context: CallbackContext) -> str:
     """Select an action"""
-    text = (
-        "Choose action"
-    )
-
     buttons = [
         [
             InlineKeyboardButton("Create shedule", 
@@ -44,79 +42,103 @@ def start(update: Update, context: CallbackContext) -> str:
     ]
     keyboard = InlineKeyboardMarkup(buttons)
 
-    # If we're starting over we don't need to send a new message
-    if context.user_data.get(START_OVER):
-        update.callback_query.answer()
-        update.callback_query.edit_message_text(text=text, reply_markup=keyboard)
-    else:
-        update.message.reply_text(
-            "Hi, i will help you to shedule questions and store answers"
-        )
-        update.message.reply_text(text=text, reply_markup=keyboard)
+    update.message.reply_text(text="Select an action", reply_markup=keyboard)
 
-    context.user_data[START_OVER] = False
     return SELECTING_ACTION
 
 
-def create_shedule(update: Update, _: CallbackContext) -> str:
-    query = update.callback_query
-    query.answer()
-    query.edit_message_text(text="Creating shedule. Type name")
-    return SET_NAME
-
-def set_name(update: Update, _: CallbackContext) -> str:
-    name_str = update.message.text
-    print(name_str)
-    update.message.reply_text(f"Setting name to {name_str}")
-    update.message.reply_text("Set time")
-    return SET_TIME
-
-def set_time(update: Update, _: CallbackContext) -> str:
+def set_time(update: Update, context: CallbackContext) -> str:
+    print(context.user_data['id'])
     time_str = update.message.text
     print(time_str)
     update.message.reply_text(f"Setting time to {time_str}")
     update.message.reply_text("Select question")
     return SET_QUESTION
 
-def set_question(update: Update, _: CallbackContext) -> str:
+def set_question(update: Update, context: CallbackContext) -> str:
+    print(context.user_data['id'])
     time_str = update.message.text
     print(time_str)
     update.message.reply_text(f"Setting question to {time_str}")
     update.message.reply_text("done")
     return STOPPING
 
-def pause_shedule(update: Update, _: CallbackContext) -> str:
+def pause_shedule(update: Update, context: CallbackContext) -> str:
+    print(context.user_data['id'])
     query = update.callback_query
     query.answer()
     query.edit_message_text(text="Pausing shedule. Choose shedule")
     return CHOOSE_NAME
 
-def choose_name(update: Update, _: CallbackContext) -> str:
+def choose_name(update: Update, context: CallbackContext) -> str:
+    print(context.user_data['id'])
     name_str = update.message.text
     print(name_str)
     update.message.reply_text(f"Choosing name {name_str}")
     update.message.reply_text("Done")
     return STOPPING
 
-def download(update: Update, _: CallbackContext) -> str:
+def download(update: Update, context: CallbackContext) -> str:
+    print(context.user_data['id'])
     query = update.callback_query
     query.answer()
     query.edit_message_text(text="Download results. Choose shedule")
     return CHOOSE_NAME
+
+class Job:
+    def __init__(self, uid: int) -> None:
+        self.uid: int = uid
+        self.name: str = ""
+        self.chat_id: int = 0
+
+    def set_name(self, name: str) -> None:
+        self.name = name
+
+    def set_chat_id(self, chat_id: int) -> None:
+        self.chat_id = chat_id
+
+    def dump(self) -> None:
+        print("=== Job ===")
+        print(f"uid: {self.uid}")
+        print(f"cid: {self.chat_id}")
+        print(f"name: {self.name}")
 
 
 class CollectorBot:
     def __init__(self, token: str) -> None:
         self.updater = Updater(token = token, use_context=True)
         self.dispatcher = self.updater.dispatcher
+        self.jobs: Dict[int, Job] = dict()
+
+    def create_job(self, update: Update, context: CallbackContext) -> str:
+        uid = uuid.uuid4().int
+        context.user_data['id'] = uid
+        self.jobs[uid] = Job(uid)
+        query = update.callback_query
+        query.answer()
+        query.edit_message_text(text="Type name to job")
+        return SET_NAME
+
+    def set_job_name(self, update: Update, context: CallbackContext) -> str:
+        uid = context.user_data["id"]
+        chat_id = update.message.chat_id
+        job_name = update.message.text
+        cur_job = self.jobs[uid]
+        cur_job.set_chat_id(chat_id)
+        cur_job.set_name(job_name)
+        for job_key in self.jobs:
+            self.jobs[job_key].dump()
+        update.message.reply_text(f"Setting name to {job_name}")
+        update.message.reply_text("Set time")
+        return SET_TIME
 
     def start(self) -> None:
 
         create_handler = ConversationHandler(
-            entry_points=[CallbackQueryHandler(create_shedule, 
+            entry_points=[CallbackQueryHandler(self.create_job, 
                                                pattern = f'^{CREATE}$')],
             states={
-                SET_NAME: [MessageHandler(Filters.text, set_name)],
+                SET_NAME: [MessageHandler(Filters.text, self.set_job_name)],
                 SET_TIME: [MessageHandler(Filters.text, set_time)],
                 SET_QUESTION: [MessageHandler(Filters.text, set_question)],
             },
